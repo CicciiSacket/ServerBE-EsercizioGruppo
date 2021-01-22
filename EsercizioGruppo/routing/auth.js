fa7ef80bffe0c58654e095d6c17d6df539f4744d
Studente <studente@MBP-di-Admin.homenet.telecomitalia.it>
2021-01-22 08:51:32 +0100

    commit

--- EsercizioGruppo/routing/auth.js
+++ EsercizioGruppo/routing/auth.js
@@ -6,7 +6,10 @@ var redis = require('redis')
 var client = redis.createClient()
 const bluebird = require('bluebird')
 bluebird.promisifyAll(redis.RedisClient.prototype)
+
+
 users = []
+
 router.post('/register', ({body},res) => {
         var user = {
             username:body.username,
@@ -15,32 +18,25 @@ router.post('/register', ({body},res) => {
         users.push(user)
         res.json({user})
     })
-router.post("/login",async ({body,headers}, res) =>{
-    if(!headers.token)return res.status(401).json({message: 'token required'})
-    var headerToken = headers.token
-    var index =users.findIndex(item=> item.username == body.username && item.password == body.password)
-    if((index != -1) && (await client.existsAsync(headerToken))){
-        users[index] = {
-            username: users[index].username,
-            password: users[index].password,
-            id: headerToken
-        }
-        client.expire(headerToken, 1200)
+router.post("/login",async ({body: {username, password},headers:{authorization}}, res) =>{
+    if(!authorization)return res.status(401).json({message: 'token required'})
+    var index =users.findIndex(item=> item.username == username && item.password == password)
+    if(index == 1) return res.status(404).json("user not found")
+    users[index] = {username,password}
+    if((await client.existAsync(authorization))){
+        users[index] = {...users[index], authorization}
+        client.expire(authorization, 1200)
         res.json(users[index])
     }
-    else if((index != -1 && !(await client.existsAsync(headerToken)))){
-    var logInToken = token.generate()
-        users[index] = {
-            username: users[index].username,
-            password: users[index].password,
-            id: logInToken
-        }
-        await client.setAsync(logInToken, users[index].username)
-        client.expire(logInToken, 1200)
+    else{
+        var logInToken = Token.generate
+        users[index] = {...users[index], authorization: logInToken}
+        await client.setAsync(logInToken, username)
+        client.expire(authorization, 1200)
         res.json(users[index])
     }
-    else res.status(404).json("user not found")
 })
+
 // var checkTokenHeader = (req,res,next)=>{
 //     const userToken = req.header("id")
 //     if(user.find(item => item.id===userToken)){
@@ -49,5 +45,6 @@ router.post("/login",async ({body,headers}, res) =>{
 //         res.status(401).json('not valid')
 //     }
 // }
+
 module.exports = router
 module.exports.users = users
