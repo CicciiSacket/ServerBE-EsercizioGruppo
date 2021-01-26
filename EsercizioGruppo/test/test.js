502b1028e7e18d6279ef1d168bbacdbaee00bee1
Studente <studente@MBP-di-Admin.homenet.telecomitalia.it>
2021-01-26 09:16:56 +0100

    lastC

--- EsercizioGruppo/test/test.js
+++ EsercizioGruppo/test/test.js
@@ -5,20 +5,19 @@ const app = require('../app.js');
 describe('POST', ()=>{ //test: OK
     it('/auth/register', async ()=>{
         const {body,status} = await request(app).post('/auth/register').set('Accept', 'application/json').send({username:'pippo@mail.com',password:'pluto'})
-        console.log(body)
+        // console.log(body)
         status.should.equal(200) 
     });
     it('/auth/login', async ()=>{
         const {body,status} = await request(app).post('/auth/login').set('Accept', 'application/json').set('authorization',"").send({username:'pippo@mail.com',password:'pluto'})
-        console.log(body)
+        // console.log(body)
         status.should.equal(201) 
     });
     it('/auth/login con generazione di nuovo token accesso', async ()=>{
         const {body,status} = await request(app).post('/auth/login').set('Accept', 'application/json').set('authorization',"2jMLxUCCTeEZWD4G7Mw4JM").send({username:'pippo@mail.com',password:'pluto'})
-        console.log(body)
+        // console.log(body)
         status.should.equal(201) 
     });
-
 });
 
 describe('GET',  ()=>{//test: OK
