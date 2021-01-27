aba37d7bcfcd260c17532d54a43eb2d5952d95dc
Studente <studente@MBP-di-Admin.homenet.telecomitalia.it>
2021-01-27 08:22:40 +0100

    LastCommit

--- EsercizioGruppo/test/test.js
+++ EsercizioGruppo/test/test.js
@@ -10,30 +10,30 @@ describe('POST', ()=>{ //test: OK
     });
     it('/auth/login', async ()=>{
         const {body,status} = await request(app).post('/auth/login').set('Accept', 'application/json').set('authorization',"").send({username:'pippo@mail.com',password:'pluto'})
-        // console.log(body)
+        console.log(body)
         status.should.equal(201) 
     });
     it('/auth/login con generazione di nuovo token accesso', async ()=>{
-        const {body,status} = await request(app).post('/auth/login').set('Accept', 'application/json').set('authorization',"2jMLxUCCTeEZWD4G7Mw4JM").send({username:'pippo@mail.com',password:'pluto'})
-        // console.log(body)
+        const {body,status} = await request(app).post('/auth/login').set('Accept', 'application/json').set('authorization',"MHMvNMPJaZWLsRzuXszWrb").send({username:'pippo@mail.com',password:'pluto'})
+        console.log(body)
         status.should.equal(201) 
     });
 });
 
 describe('GET',  ()=>{//test: OK
     it('/products ', async () => {
-        const {status, body} =  await request(app).get('/products').set('Accept', 'application/json').set('authorization',"Wj7dfRSHpeBwxL1X4FpNjj")
+        const {status, body} =  await request(app).get('/products').set('Accept', 'application/json').set('authorization',"MHMvNMPJaZWLsRzuXszWrb")
         status.should.equal(200);
     });
     it('/products/:id', async () =>{
-        const {status} = await request(app).get('/products/5296').set('Accept', 'application/json').set('authorization',"Wj7dfRSHpeBwxL1X4FpNjj")
+        const {status} = await request(app).get('/products/5296').set('Accept', 'application/json').set('authorization',"MHMvNMPJaZWLsRzuXszWrb")
         status.should.equal(200)
     }); 
 })
 
 describe('DELETE', ()=>{//test: OK
     it('/auth/logout', async () =>{
-        const {status} = await request(app).delete('/auth/logout').set('Accept', 'application/json').set('authorization',"Wj7dfRSHpeBwxL1X4FpNjj")
+        const {status} = await request(app).delete('/auth/logout').set('Accept', 'application/json').set('authorization',"MHMvNMPJaZWLsRzuXszWrb")
         status.should.equal(200)
     });
 })
